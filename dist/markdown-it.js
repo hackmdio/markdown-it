d42c5b1d84979e056bf3501d794f53a9244274d4
Yukai Huang <yukaihuangtw@gmail.com>
2020-05-14 14:20:26 +0800

    Bump version to 10.0.0-pre9

--- dist/markdown-it.js
+++ dist/markdown-it.js
@@ -1,4 +1,4 @@
-/*! @hackmd/markdown-it 10.0.0-pre8 https://github.com//markdown-it/@hackmd/markdown-it @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownit = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+/*! @hackmd/markdown-it 10.0.0-pre9 https://github.com//markdown-it/@hackmd/markdown-it @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownit = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // HTML5 entities map: { name -> utf16string }
 //
 'use strict';
@@ -4334,7 +4334,13 @@ module.exports = function inline(state, positionOffset) {
     tok.position += positionOffset || 0;
 
     if (tok.type === 'inline') {
-      state.md.inline.parse(tok.content, state.md, Object.assign({}, state.env, { parentToken: tok }), tok.children);
+      state.md.inline.parse(tok.content, state.md,
+        Object.assign(
+          {},
+          state.env,
+          { parentToken: tok, parentState: state, parentTokenIndex: i }
+        ),
+        tok.children);
 
       // Update position of all children to be absolute
       for (var child = 0; child < tok.children.length; child++) {
